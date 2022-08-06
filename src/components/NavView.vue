898c91566f2385aae35fe7ecef62f474c777b91e
shane <shanestevensxk9@gmail.com>
2022-08-06 14:36:54 +0200

    update

---
+++ src/components/NavView.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/landing">Landing</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/store">Shop</router-link> |
+    <router-link to="/single">single</router-link> |
+    <router-link to="/admin">Admin</router-link> |
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/landing',
+    name: 'landing',
+    component: () => import('../views/LandingView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/store',
+    name: 'store',
+    component: () => import('../views/StoreView.vue')
+  },
+  {
+    path: '/single',
+    name: 'single',
+    component: () => import('../views/SingleView.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('../views/AdminView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
