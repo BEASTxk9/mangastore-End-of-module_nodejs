5d2d0603ad6a89992f2f5cc077f3c0a77e194cad
shane <shanestevensxk9@gmail.com>
2022-08-10 14:24:50 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,71 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
+// Picknpay url
+const picknpayUrl = "https://mangastore-end-of-module.herokuapp.com/";
 
 export default createStore({
   state: {
+    users: null,
+    products: null
   },
   getters: {
+    getUsers: state => state.users,
+    getProducts: state => state.products
   },
   mutations: {
+    setUsers(state, values) {
+      state.users = values
+    },
+
+    setproducts: (state, products) => {
+      state.products = products
+    },
   },
   actions: {
+    // get users
+fetchUsers: async (content) => {
+  let res = await axios.post(picknpayUrl+"register", data);
+  let {results } = await res.data;
+  if(results) {
+    content.commit('setUsers', results);
+  }else{
+    console.log("There is no data");
+  }
+},
+
+    //Signup
+    signUp: async (context, playload)=> {
+      let {firstname, lastname, gender, address, userole, email, userpassword} = playload;
+      const data = {
+        firstname, 
+        lastname, 
+        gender,
+        address,
+        userole, 
+        email,
+        userpassword
+      };
+      let res = await axios.post(picknpayUrl+"register", data);
+      let results = await res.data;
+      console.log(results);
+      if(results) {
+        context.commit('setUsers', results);
+      }else {
+        console.error("No dota");
+      }
+    },
+
+// get products
+    getproducts: async (context) => {
+      let res = await fetch('https://mangastore-end-of-module.herokuapp.com/products');
+      let data = await res.json();
+      let result = data.results;
+      if(result){
+        context.commit('setproducts', result)
+      }else{
+        console.log('loading...')
+      }
+    },
   },
   modules: {
   }
