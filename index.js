2e297b3125ceaf51c489b496963373342009ccd0
shane <shanestevensxk9@gmail.com>
2022-08-10 13:26:24 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const { compare, hash } = require('bcrypt');
 const app = express();
 // express router
 const router = express.Router();
-// cors app
+
+
 
 // Configuration
 const port = parseInt(process.env.PORT) || 4000;
@@ -20,6 +21,7 @@ const port = parseInt(process.env.PORT) || 4000;
 // allow access to fetch data from the api externally by  Seting header
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
@@ -93,6 +95,83 @@ router.get('/products/:id', (req, res) => {
     });
 
 });
+
+// ___________________
+// login & register
+db.connect( (err) => {
+    if(err){
+        console.log(err)
+    } else{
+        console.log('mySQL connected...')
+    }
+});
+
+// Register
+app.post('/register',bodyParser.json(), 
+    async (req, res)=> {
+    try{
+        const bd = req.body; 
+     
+        // Encrypting a password
+        // Default value of salt is 10. 
+        bd.password = await hash(bd.password, 11);
+        // Query
+
+        const strQry = 
+        `
+        INSERT INTO users(fullname, email, password, userRole, phone_number, join_date)
+        VALUES(?, ?, ?, ?, ?, ?);
+        `;
+        db.query(strQry, 
+            [bd.fullname, bd.email, bd.password, bd.userRole,bd.phone_number, bd.join_date],
+            (err, results)=> {
+                if(err) {
+                    console.log(err)
+                } else{
+                    console.log(results);
+                }
+            })
+    }catch(e) {
+        console.log(`From registration: ${e.message}`);
+    }
+});
+
+// Login
+app.post('/login', bodyParser.json(),
+    (req, res)=> {
+    try{
+
+        // Get email and password
+        const { email, password } = req.body;
+        const strQry = 
+        `
+        SELECT email, password
+        FROM users 
+        WHERE email = '${email}';
+        `;
+        db.query(strQry, async (err, results)=> {
+            if(err) throw err;
+
+            switch(true){
+                case (await compare(password,results[0].password)):
+                    res.send("alert('Well done')");
+                // res.send("Welcome "+results[0].firstname)
+                break
+                default: 
+                console.log("Bye");
+            }
+
+            // res.json({
+            //     status: 200,
+            //     results: (await compare(userpassword,
+            //         results[0].userpassword)) ? results : 
+            //         'You provided a wrong email or password'
+            // })
+        })
+    }catch(e) {
+        console.log(`From login: ${e.message}`);
+    }
+})
 
 console.log(bodyParser.json())
 
