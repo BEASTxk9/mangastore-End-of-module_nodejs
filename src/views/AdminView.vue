8472788651073151aa335ccd57a1039f4103e74b
shane <shanestevensxk9@gmail.com>
2022-08-10 15:44:00 +0200

    update

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -5,11 +5,11 @@
 
 <!-- nav -->
     <div class="row justify-content-center pt-3">
-        <div id="sidenav" class="col-sm-1 logo">
+        <div id="sidenav" class="col-sm-1">
             <p>ADMIN</p>
         </div>
 
-        <div id="table" class="col-sm-10">
+        <div id="table" class="col-sm-11">
 
   <h6 id="logo" class="py-1 text-end">
            LOGO UNDECIDED YET
@@ -30,9 +30,9 @@
 </nav>
 </div>
 <!-- table -->
-<div id="table" class="col-sm-10">
-<table class="table table-border">
-    <thead>
+<div class="col-sm-12">
+<table class="table table-bordered">
+    <thead class="table-bordered">
         <th>ID</th>
         <th>Title</th>
         <th>bookName</th>
@@ -52,7 +52,7 @@
     <td><img class="rounded text-center justify-content-center mx-1" :src="product.img" alt="" defer></td>
     <td>{{product.category}}</td>
     <td id="description">{{product.description}}</td>
-      <td>{{product.price}}</td>
+      <td>R{{product.price}}</td>
         <td>{{product.datereleased}}</td>
           <td>{{product.created_by}}</td>
       
@@ -89,19 +89,11 @@ mounted() {
 
 <style scoped>
 #table{
-    border-left: 0.5px solid rgba(128, 128, 128, 0.282);
     border-top: 0.5px solid rgba(128, 128, 128, 0.282);
-    border-right: 0.5px solid rgba(128, 128, 128, 0.282);
-}
-
-thead{
-    border-bottom: 0.5px solid rgba(128, 128, 128, 0.282);
-    font-size: 1.5rem;
 }
 
 #sidenav{
        border-top: 0.5px solid rgba(128, 128, 128, 0.282);
-           border-left: 0.5px solid rgba(128, 128, 128, 0.282);
 }
 
 img{
