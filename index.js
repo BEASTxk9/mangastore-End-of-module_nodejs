5bdd407c54688d6fa80563a69a4a9b60d271c844
shane <shanestevensxk9@gmail.com>
2022-08-12 09:51:01 +0200

    update

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const port = parseInt(process.env.PORT) || 4000;
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    // res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
@@ -221,7 +221,6 @@ app.post('/login', bodyParser.json(),
             switch(true){
                 case (await compare(password,results[0].password)):
                     res.redirect('/products')
-                    // res.send(`<a href="/products">Welcom. Click...</a>`);
                 // res.send("Welcome "+results[0].firstname)
                 break
                 default: 
