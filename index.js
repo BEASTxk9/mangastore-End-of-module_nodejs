29e7749521479e37263144672d7472b43a4ea0c3
shane <shanestevensxk9@gmail.com>
2022-08-15 13:33:53 +0200

    update

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const port = parseInt(process.env.PORT) || 4000;
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
     next();
 });
 
