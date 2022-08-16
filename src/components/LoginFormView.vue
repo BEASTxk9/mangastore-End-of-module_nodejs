126ade6fc43771e57b111850e4526d5b478fc072
shane <shanestevensxk9@gmail.com>
2022-08-16 09:57:58 +0200

    update

--- src/components/LoginFormView.vue
+++ src/components/LoginFormView.vue
@@ -16,7 +16,9 @@
 <label for="password">Password</label><br>
     <input type="password" v-model="password" minlength="8" maxlength="15" placeholder="Enter your password"  required><br>
 
-    <button class="mt-4" v-on:click="submit">Login</button>
+   <button type="submit" class="button" onclick="this.classList.toggle('button--loading')">
+    <span class="button__text">Login</span>
+</button>
 
 </form>
 
@@ -49,5 +51,56 @@ this.$store.dispatch('login', {
 </script>
 
 <style scoped>
+/* button */
+.button {
+  position: relative;
+  margin-top: 10px;
+  padding: 8px 16px;
+  background: #009579;
+  border: none;
+  outline: none;
+  border-radius: 2px;
+  cursor: pointer;
+}
 
+.button:active {
+  background: #007a63;
+}
+
+.button__text {
+  font: bold 20px "Quicksand", san-serif;
+  color: #ffffff;
+  transition: all 0.2s;
+}
+
+.button--loading .button__text {
+  visibility: hidden;
+  opacity: 0;
+}
+
+.button--loading::after {
+  content: "";
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  border: 4px solid transparent;
+  border-top-color: #ffffff;
+  border-radius: 50%;
+  animation: button-loading-spinner 1s ease infinite;
+}
+
+@keyframes button-loading-spinner {
+  from {
+    transform: rotate(0turn);
+  }
+
+  to {
+    transform: rotate(1turn);
+  }
+}
 </style>
